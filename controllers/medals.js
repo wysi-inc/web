0496280f8e4d730e428735ac88f433703536e9a1
T A K U <83608709+mrtaku@users.noreply.github.com>
2023-10-20 11:46:34 +0200

    medal.js comments

--- controllers/medals.js
+++ controllers/medals.js
@@ -1,7 +1,10 @@
+// This function retrieves medal data from an external API and returns it as a JSON response.
 export const getMedals = async (req, res) => {
+  // Send a GET request to an external API endpoint that provides medal data
   const ans = await (
     await fetch("https://osekai.net/medals/api/medals.php")
   ).json();
 
+  // Return the retrieved data as a JSON response
   return res.json(ans);
 };
