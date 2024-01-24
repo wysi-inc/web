673836502943c7c6f773e19f21a9b3dd4ccabb07
M4rti21 <m4rti21@proton.me>
2024-01-25 00:10:18 +0100

    login started

--- src/controllers/web.ts
+++ src/controllers/web.ts
@@ -1,3 +1,40 @@
-export function login() {
-    return "Login Page";
+export async function login(jwt, cookie, setCookie, params, request) {
+    const { code } = params;
+    const user = await getOwnData(await validateCode(code));
+    if (user.authentication) return "Unauthorized";
+    const jwtUser = {
+        ip: request.ip,
+        id: user.id,
+        username: user.username,
+        avatar: user.avatar_url,
+    };
+    setCookie('auth', await jwt.sign(jwtUser), {
+        httpOnly: true,
+        maxAge: 7 * 86400,
+    })
+    return `Sign in as ${user.username}`
+}
+
+async function getOwnData(token: string) {
+  return await (
+    await fetch("https://osu.ppy.sh/api/v2/me/osu", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  ).json();
+}
+
+async function validateCode(code: string) {
+    return (await(await fetch("https://osu.ppy.sh/oauth/token", {
+        method: "POST", headers: {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `client_id=${process.env.OSU_ID}&client_secret=${process.env.OSU_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${process.env.OSU_REDIRECT}`,
+    })
+    ).json()).access_token;
 }
