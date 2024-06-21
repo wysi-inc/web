5bef18f2fd5c93611302cc8a4dcad1af763db382
M4rti21 <m4rti21@proton.me>
2024-06-21 02:11:11 +0200

    lighthouse

--- src/components/web/Home.tsx
+++ src/components/web/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     return <div class="flex flex-col gap-4">
         <div class="flex flex-col gap-2 items-center">
             <h1 class="text-2xl text-center">Welcome to wysi727.com!</h1>
-            <img src="/public/wysi.svg" class="rounded-xl size-24 shadow-lg" />
+            <img src="/public/wysi.svg" class="rounded-xl size-24 shadow-lg" alt="wysi logo" />
             <h2 class="text-lg text-center">An <b>open source</b> osu website <i>*alternative*</i></h2>
             <h3 class="text-sm text-center">Built with bun and htmx</h3>
             <div />
@@ -12,25 +12,25 @@ const Home = () => {
             <div class="bg-base-100 rounded-lg flex flex-col">
                 <h1 class="p-2 text-neutral-content">User Profiles</h1>
                 <div class="flex flex-col gap-2 p-2 bg-base-300 rounded-lg grow">
-                    <img src="/public/img/home/profile_1.png" />
+                    <img src="/public/img/home/profile_1.png" alt="user profiles" />
                 </div>
             </div>
             <div class="bg-base-100 rounded-lg flex flex-col">
                 <h1 class="p-2 text-neutral-content">Beatmap Search</h1>
                 <div class="flex flex-col gap-2 p-2 bg-base-300 rounded-lg grow">
-                    <img src="/public/img/home/beatmaps.png" />
+                    <img src="/public/img/home/beatmaps.png" alt="beatmaps" />
                 </div>
             </div>
             <div class="bg-base-100 rounded-lg flex flex-col">
                 <h1 class="p-2 text-neutral-content">PP Calculation</h1>
                 <div class="flex flex-col gap-2 p-2 bg-base-300 rounded-lg grow">
-                    <img src="/public/img/home/pp_calculator.png" />
+                    <img src="/public/img/home/pp_calculator.png" alt="pp calculator" />
                 </div>
             </div>
             <div class="bg-base-100 rounded-lg flex flex-col">
                 <h1 class="p-2 text-neutral-content">And more!!</h1>
                 <div class="flex flex-col gap-2 p-2 bg-base-300 rounded-lg grow">
-                    <img class="blur" src="/public/img/home/profile_2.png" />
+                    <img class="blur" src="/public/img/home/profile_2.png" alt="more stuff" />
                 </div>
             </div>
 
