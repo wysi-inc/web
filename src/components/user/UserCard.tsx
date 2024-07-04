251017d53f071280655733358379dae9404f7a7a
M4rti21 <m4rti21@proton.me>
2024-07-05 01:22:11 +0200

    usercard redirects to wysi

--- src/components/user/UserCard.tsx
+++ src/components/user/UserCard.tsx
@@ -2,6 +2,7 @@ import { v2 } from "osu-api-extended";
 import Flag from "./u_panels/u_components/Flag";
 import SubdivisionFlag from "./u_panels/u_components/SubdivisionFlag";
 import Clan from "./u_panels/u_components/Clan";
+import Link from "../web/Link";
 
 type Props = {
     user_id: number
@@ -18,8 +19,7 @@ async function UserCard(p: Props) {
             <img src={user.avatar_url} alt="pfp" loading="lazy" class="size-12 rounded-lg" />
             <div class="flex flex-row gap-2 items-center">
                 <Clan user_id={user.id} />
-                <a href={`https://osu.ppy.sh/users/${user.id}`}
-                    target="_blank" class="text-xl underline-offset-2 hover:underline">{user.username}</a>
+                <Link url={`/users/${user.id}`} css="text-xl">{user.username}</Link>
                 <Flag name={user.country.name} code={user.country.code} />
                 <SubdivisionFlag user_id={user.id} />
             </div>
