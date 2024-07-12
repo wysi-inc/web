b1a0a7e03503ca06a14447978952a309e41e516f
M4rti21 <m4rti21@proton.me>
2024-07-13 00:41:44 +0200

    a

--- src/components/user/UserPage.tsx
+++ src/components/user/UserPage.tsx
@@ -140,7 +140,7 @@ const UserPage = async (p: Props) => {
         <Title title={user.username} />
         <UserTopPanel t={p.t} user={user} mode={p.mode} editable={editable} />
         <nav class="underline-offset-1 text-neutral-content sticky -mt-8 top-16 bg-base-300 md:rounded-lg shadow-lg p-2 z-40 flex items-center justify-center flex-row gap-6 flex-wrap">
-            <a class="hover:underline" href="#top">Top</a>
+            <a class="hover:underline" href="#top">{p.t.user.sections.top}</a>
             {panels.map((p) => {
                 if (!p.show_if) return <></>;
                 return <a class="hover:underline" href={`#${p.code}`}>{p.title}</a>
