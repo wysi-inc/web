b1bff368a55db32c7ad33e5e3f558a5c2d01a72a
M4rti21 <m4rti21@proton.me>
2024-08-03 17:43:31 +0200

    fix score panel i messed up for some reason

--- src/components/beatmap/BeatmapScoreTable.tsx
+++ src/components/beatmap/BeatmapScoreTable.tsx
@@ -1,15 +1,15 @@
 import type { Mod, Mode } from "@/src/types/osu";
 import { v2 } from "osu-api-extended";
-import type { response } from "osu-api-extended/dist/types/v2_scores_beatmap";
 import moment from "moment";
+import type { response } from "osu-api-extended/dist/types/v2_scores_beatmap";
 import Grade from "../score/Grade";
 import ModIcon from "../score/ModIcon";
 import Flag from "../user/u_panels/u_components/Flag";
 import SubdivisionFlag from "../user/u_panels/u_components/SubdivisionFlag";
 import Link from "../web/Link";
 import Clan from "../user/u_panels/u_components/Clan";
-// import { colors } from "@/src/libs/colors";
 import type { v2Score } from "@/src/types/beatmaps";
+import { colors } from "@/src/libs/colors";
 
 type ActualStatistics = response & {
     statistics: {
@@ -34,6 +34,8 @@ const BeatmapScoreTable = async (p: {
     const mods = Object.entries(p.body);
     const mod_names = mods.map(([name, value]) => value === 'on' ? name.split("-")[1] : null).filter(v => v !== null) as Mod[];
 
+    console.log(p.b_id);
+
     const scores: ActualStatistics[] = await v2.scores.beatmap(p.b_id, {
         mode: p.mode,
         mods: mod_names,
@@ -49,25 +51,12 @@ const BeatmapScoreTable = async (p: {
             mods: mod_names,
             best_only: true
         }) as any;
+
     }
 
-
     return (<>
-        <div class="rounded-lg"
-            style={{
-                backgroundImage: `url(${scores[0].user.cover.url})`,
-                backgroundSize: `cover`,
-                backgroundPosition: `center`,
-                backgroundRepeat: "no-repeat"
-            }}>
-        </div>
-        {
-            //     <BigScore score={scores[0]} />
-            // {user_score ?
-            //     <BigScore score={user_score} /> : <></>
-            // }
-        }
-        <table class="table table-xs table-zebra p-4 bg-base-300 rounded-lg">
+        <BigScore score={scores[0]} />
+        <table class="table table-xs table-zebra bg-base-300 rounded-lg">
             <thead>
                 <tr>
                     <th></th>
@@ -94,30 +83,10 @@ const BeatmapScoreTable = async (p: {
                             </div>
                         </td>
                         <td class="hidden sm:table-cell">
-                            {Number(score.pp?.toFixed()).toLocaleString()}pp</td>
+                            {Number((score.pp?.toFixed()) || 0).toLocaleString()}pp</td>
                         <td class="hidden md:table-cell">
                             {(score.accuracy * 100).toFixed(2)}%
                         </td>
-                        {/*<td class="hidden md:table-cell">
-                            <div class="grid grid-cols-4 gap-4 px-2 bg-base-300 rounded-full">
-                                <span class={`text-base-content ${score.statistics.great ? "" : "text-opacity-50"}`}
-                                    style={{ color: score.statistics.great ? colors.judgements.x300 : "" }}>
-                                    {score.statistics.great || 0}
-                                </span>
-                                <span class={`text-base-content ${score.statistics.ok ? "" : "text-opacity-50"}`}
-                                    style={{ color: score.statistics.ok ? colors.judgements.x100 : "" }}>
-                                    {score.statistics.ok || 0}
-                                </span>
-                                <span class={`text-base-content ${score.statistics.meh ? "" : "text-opacity-50"}`}
-                                    style={{ color: score.statistics.meh ? colors.judgements.x50 : "" }}>
-                                    {score.statistics.meh || 0}
-                                </span>
-                                <span class={`text-base-content ${score.statistics.miss ? "" : "text-opacity-50"}`}
-                                    style={{ color: score.statistics.miss ? colors.judgements.xMiss : "" }}>
-                                    {score.statistics.miss || 0}
-                                </span>
-                            </div>
-                        </td>*/}
                         <td class="hidden md:table-cell">
                             {score.is_perfect_combo ?
                                 <span class="text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-sky-500">
@@ -153,103 +122,111 @@ const BeatmapScoreTable = async (p: {
     </>);
 };
 
-// function BigScore(p: { score: v2Score | ActualStatistics }) {
-//     return (
-//         <div class="text-base-content bg-base-301 bg-opacity-65 backdrop-blur-sm justify-between flex flex-row flex-wrap gap-4 p-4 rounded-lg">
-//             <div class="flex flex-row flex-wrap gap-4 items-center">
-//                 <div class="flex flex-col gap-2 items-center">
-//                     <span class="text-xl">#{p.score.position}</span>
-//                     <Grade grade={p.score.rank} />
-//                 </div>
-//                 <img loading="lazy" src={p.score.user.avatar_url} alt="pfp" class="size-20 rounded-lg" />
-//                 <div class="flex flex-col items-start gap-1">
-//                     <div class="flex flex-row gap-2 text-xl items-center">
-//                         <Flag name={p.score.user.country.name} code={p.score.user.country.code} />
-//                         <Link url={`/users/${p.score.user.id}`}>{p.score.user.username}</Link>
-//                     </div>
-//                     <span class="tooltip" data-tip={`${moment(p.score.ended_at).format("MMMM Do YYYY")} | ${moment(p.score.ended_at).fromNow()}`}>
-//                         {moment(p.score.ended_at).fromNow()}
-//                     </span>
-//                 </div>
-//             </div>
-//             <div class="flex flex-col gap-2 items-end">
-//                 <dl class="flex flex-row gap-8 justify-end items-top">
-//                     <div class="flex flex-col">
-//                         <dt class="text-xs">Score</dt>
-//                         <dd class="text-lg">{p.score.total_score.toLocaleString()}</dd>
-//                     </div>
-//                     <div class="flex flex-col">
-//                         <dt class="text-xs">Accuracy</dt>
-//                         {p.score.accuracy === 1 ?
-//                             <dd class="text-lg text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-sky-500">
-//                                 {(p.score.accuracy * 100).toFixed(2)}%
-//                             </dd> :
-//                             <dd>
-//                                 {(p.score.accuracy * 100).toFixed(2)}%
-//                             </dd>
-//                         }
-//                     </div>
-//                     <div class="flex flex-col">
-//                         <dt class="text-xs">Max Combo</dt>
-//                         {p.score.is_perfect_combo ?
-//                             <dd class="text-lg text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-sky-500">
-//                                 {p.score.max_combo.toLocaleString()}x
-//                             </dd> :
-//                             <dd>
-//                                 {p.score.max_combo.toLocaleString()}x
-//                             </dd>
-//                         }
-//                     </div>
-//                 </dl>
-//                 <div class="flex flex-row gap-4 justify-end items-center">
-//                     <dl class="flex flex-row gap-8 justify-end items-top">
-//                         <div class="flex flex-col">
-//                             <dt class="text-xs">300</dt>
-//                             <dd class={`text-lg text-base-content ${p.score.statistics.great ? "" : "text-opacity-50"}`}
-//                                 style={{ color: p.score.statistics.great ? colors.judgements.x300 : "" }}>
-//                                 {p.score.statistics.great || 0}
-//                             </dd>
-//                         </div>
-//                         <div class="flex flex-col">
-//                             <dt class="text-xs">100</dt>
-//                             <dd class={`text-lg text-base-content ${score.statistics.ok ? "" : "text-opacity-50"}`}
-//                                 style={{ color: p.score.statistics.ok ? colors.judgements.x100 : "" }}>
-//                                 {p.score.statistics.ok || 0}
-//                             </dd>
-//                         </div>
-//                         <div class="flex flex-col">
-//                             <dt class="text-xs">50</dt>
-//                             <dd class={`text-lg text-base-content ${score.statistics.meh ? "" : "text-opacity-50"}`}
-//                                 style={{ color: p.score.statistics.meh ? colors.judgements.x50 : "" }}>
-//                                 {p.score.statistics.meh || 0}
-//                             </dd>
-//                         </div>
-//                         <div class="flex flex-col">
-//                             <dt class="text-xs">Miss</dt>
-//                             <dd class={`text-lg text-base-content ${score.statistics.miss ? "" : "text-opacity-50"}`}
-//                                 style={{ color: p.score.statistics.miss ? colors.judgements.xMiss : "" }}>
-//                                 {p.score.statistics.miss || 0}
-//                             </dd>
-//                         </div>
-//                         <div class="flex flex-col">
-//                             <dt class="text-xs">Performance</dt>
-//                             <dd class="text-lg">
-//                                 {Math.round(p.score.pp)}pp
-//                             </dd>
-//                         </div>
-//                         <div class="flex flex-col">
-//                             <dt class="text-xs">Mods</dt>
-//                             <dd class="mt-1 text-lg flex flex-row flex-wrap gap-1">
-//                                 {p.score.mods.map((mod) =>
-//                                     <ModIcon mod={(mod as any).acronym} />
-//                                 )}
-//                             </dd>
-//                         </div>
-//                     </dl>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+function BigScore(p: { score: ActualStatistics }) {
+    return (<>
+        <div class="rounded-lg"
+            style={{
+                backgroundImage: `url(${p.score.user.cover.url})`,
+                backgroundSize: `cover`,
+                backgroundPosition: `center`,
+                backgroundRepeat: "no-repeat"
+            }}>
+            <div class="text-base-content bg-base-301 bg-opacity-65 backdrop-blur-sm justify-between flex flex-row flex-wrap gap-4 p-4 rounded-lg">
+                <div class="flex flex-row flex-wrap gap-4 items-center">
+                    <div class="flex flex-col gap-2 items-center">
+                        <span class="text-xl">#{p.score.position}</span>
+                        <Grade grade={p.score.rank} />
+                    </div>
+                    <img loading="lazy" src={p.score.user.avatar_url} alt="pfp" class="size-20 rounded-lg" />
+                    <div class="flex flex-col items-start gap-1">
+                        <div class="flex flex-row gap-2 text-xl items-center">
+                            <Flag name={p.score.user.country.name} code={p.score.user.country.code} />
+                            <Link url={`/users/${p.score.user.id}`}>{p.score.user.username}</Link>
+                        </div>
+                        <span class="tooltip" data-tip={`${moment(p.score.ended_at).format("MMMM Do YYYY")} | ${moment(p.score.ended_at).fromNow()}`}>
+                            {moment(p.score.ended_at).fromNow()}
+                        </span>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2 items-end">
+                    <dl class="flex flex-row gap-8 justify-end items-top">
+                        <div class="flex flex-col">
+                            <dt class="text-xs">Score</dt>
+                            <dd class="text-lg">{p.score.total_score.toLocaleString()}</dd>
+                        </div>
+                        <div class="flex flex-col">
+                            <dt class="text-xs">Accuracy</dt>
+                            {p.score.accuracy === 1 ?
+                                <dd class="text-lg text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-sky-500">
+                                    {(p.score.accuracy * 100).toFixed(2)}%
+                                </dd> :
+                                <dd>
+                                    {(p.score.accuracy * 100).toFixed(2)}%
+                                </dd>
+                            }
+                        </div>
+                        <div class="flex flex-col">
+                            <dt class="text-xs">Max Combo</dt>
+                            {p.score.is_perfect_combo ?
+                                <dd class="text-lg text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-sky-500">
+                                    {p.score.max_combo.toLocaleString()}x
+                                </dd> :
+                                <dd>
+                                    {p.score.max_combo.toLocaleString()}x
+                                </dd>
+                            }
+                        </div>
+                    </dl>
+                    <div class="flex flex-row gap-4 justify-end items-center">
+                        <dl class="flex flex-row gap-8 justify-end items-top">
+                            <div class="flex flex-col">
+                                <dt class="text-xs">300</dt>
+                                <dd class={`text-lg text-base-content ${p.score.statistics.great ? "" : "text-opacity-50"}`}
+                                    style={{ color: p.score.statistics.great ? colors.judgements.x300 : "" }}>
+                                    {p.score.statistics.great || 0}
+                                </dd>
+                            </div>
+                            <div class="flex flex-col">
+                                <dt class="text-xs">100</dt>
+                                <dd class={`text-lg text-base-content ${p.score.statistics.ok ? "" : "text-opacity-50"}`}
+                                    style={{ color: p.score.statistics.ok ? colors.judgements.x100 : "" }}>
+                                    {p.score.statistics.ok || 0}
+                                </dd>
+                            </div>
+                            <div class="flex flex-col">
+                                <dt class="text-xs">50</dt>
+                                <dd class={`text-lg text-base-content ${p.score.statistics.meh ? "" : "text-opacity-50"}`}
+                                    style={{ color: p.score.statistics.meh ? colors.judgements.x50 : "" }}>
+                                    {p.score.statistics.meh || 0}
+                                </dd>
+                            </div>
+                            <div class="flex flex-col">
+                                <dt class="text-xs">Miss</dt>
+                                <dd class={`text-lg text-base-content ${p.score.statistics.miss ? "" : "text-opacity-50"}`}
+                                    style={{ color: p.score.statistics.miss ? colors.judgements.xMiss : "" }}>
+                                    {p.score.statistics.miss || 0}
+                                </dd>
+                            </div>
+                            <div class="flex flex-col">
+                                <dt class="text-xs">Performance</dt>
+                                <dd class="text-lg">
+                                    {Math.round(p.score.pp)}pp
+                                </dd>
+                            </div>
+                            <div class="flex flex-col">
+                                <dt class="text-xs">Mods</dt>
+                                <dd class="mt-1 text-lg flex flex-row flex-wrap gap-1">
+                                    {p.score.mods.map((mod) =>
+                                        <ModIcon mod={(mod as any).acronym} />
+                                    )}
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>);
+}
 
 export default BeatmapScoreTable;
--- src/routes/beatmaps.tsx
+++ src/routes/beatmaps.tsx
@@ -1,5 +1,5 @@
 import { Elysia, t } from 'elysia'
-import type { Mod, Mode, Route } from '../types/osu';
+import type { Mode, Route } from '../types/osu';
 import BeatmapsetSearch from '../components/beatmap/BeatmapsetSearch';
 import BeatmapsList from '../components/beatmap/BeatmapsList';
 import BeatmapsetPage from '../components/beatmap/BeatmapsetPage';
@@ -60,7 +60,7 @@ export const beatmapRoutes = new Elysia({ prefix: '/beatmapsets' })
         </HtmxPage>
     ))
     .post("/:set_id/:beatmap_id/scores/:mode", ({ params, body }) => (
-        <BeatmapScoreTable id={Number(params.beatmap_id)}
+        <BeatmapScoreTable b_id={Number(params.beatmap_id)}
             mode={params.mode as Mode} body={body} />
     ))
     .post("/collectioncard/:hash", ({ params }) => (
