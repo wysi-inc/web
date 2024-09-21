747a04f61d940a98a782c1f01af0f3e4126cb17b
M4rti21 <m4rti21@proton.me>
2024-09-21 13:42:42 +0200

    flag

--- src/components/user/u_panels/u_components/Flag.tsx
+++ src/components/user/u_panels/u_components/Flag.tsx
@@ -3,8 +3,10 @@ import Link from "@/src/components/web/Link";
 const Flag = (p: { name: string; code: string; }) => {
     const url = `https://flagcdn.com/h40/${p.code.toLowerCase()}.webp`;
     return (
-        <Link url={`/rankings/osu/performance?country=${p.code}`} css="tooltip flex items-center justify-center h-5 w-7" data-tip={p.name}>
-            <img src={url} alt={`${p.name}'s flag`} loading="lazy" class="max-h-5 max-w-7 rounded-sm drop-shadow-solid" />
+        <Link url={`/rankings/osu/performance?country=${p.code}`}>
+            <span class="tooltip flex items-center justify-center h-5 w-7" data-tip={p.name}>
+                <img src={url} alt={`${p.name}'s flag`} loading="lazy" class="max-h-5 max-w-7 rounded-sm drop-shadow-solid" />
+            </span>
         </Link>
     );
 }
