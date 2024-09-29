2695694da42bd14fa397814078b005fac35cb0d2
M4rti21 <m4rti21@proton.me>
2024-09-29 10:04:18 +0200

    bbcodejs

--- public/js/bbcode.js
+++ public/js/bbcode.js
@@ -1,15 +1,20 @@
-document.querySelectorAll('.js-spoilerbox')?.forEach(box => {
-    const body = box.querySelector('.js-spoilerbox__body');
-    const link = box.querySelector('.js-spoilerbox__link');
-    const icon = link.querySelector('.bbcode-spoilerbox__link-icon');
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (body.style.display === 'block') {
-            body.style.display = 'none';
-            icon.style.transform = 'rotate(90deg)';
-        } else {
-            body.style.display = 'block';
-            icon.style.transform = 'rotate(0)';
-        }
-    })
-});
+function foldSpoilerboxes() {
+    var spoilerboxes = document.querySelectorAll('.js-spoilerbox');
+    spoilerboxes?.forEach(box => {
+        const body = box.querySelector('.js-spoilerbox__body');
+        const link = box.querySelector('.js-spoilerbox__link');
+        const icon = link.querySelector('.bbcode-spoilerbox__link-icon');
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (body.style.display === 'block') {
+                body.style.display = 'none';
+                icon.style.transform = 'rotate(90deg)';
+            } else {
+                body.style.display = 'block';
+                icon.style.transform = 'rotate(0)';
+            }
+        })
+    });
+}
+
+htmxAfterFunctions.push(foldSpoilerboxes);
--- src/components/web/Link.tsx
+++ src/components/web/Link.tsx
@@ -1,17 +1,15 @@
-type Props = {
+function Link(p: {
     url: string,
     children: any,
     css?: string,
     role?: string,
     label?: string,
-}
-
-function Link(p: Props) {
+}) {
     return <a href={p.url}
-        // hx-get={p.url}
-        // hx-push-url="true" hx-target="#main"
-        // hx-swap="innerHTML show:window:top"
-        // hx-indicator="#page-loading"
+        hx-get={p.url}
+        hx-push-url="true" hx-target="#main"
+        hx-swap="innerHTML show:window:top"
+        hx-indicator="#page-loading"
         class={`${p.css || "hover:underline underline-offset-2"} cursor-pointer `}
         role={p.role} aria-label={p.label}>
         {p.children}
