27ea7f82ef4f96747233b50050792ba93677783d
M4rti21 <m4rti21@proton.me>
2024-10-13 12:55:28 +0200

    Fix Worker Threads
    Fixed PR for worker threads to save the data to the global variables.
    Also changed the `if` statments for a `switch case`.

--- src/tasks/files.ts
+++ src/tasks/files.ts
@@ -1,38 +1,33 @@
-import { readdir } from "node:fs/promises";
-import { Worker } from 'worker_threads'
-import { log } from "./logs";
+import path from "path";
+import { Worker } from "worker_threads";
 import type { LanguagesType, SubdivisionFlagsType } from "../types/countries";
-import path from 'path'
 
-const BASEDIR = ".";
 export let TRANSLATIONS: any = {};
 export let LANGUAGES: LanguagesType = {};
 export let SUBDIVISION_FLAGS: SubdivisionFlagsType = {};
 
 export function txt(lang: string, path: string): string {
-    let t = path.split('.').reduce((acc, part) => acc && acc[part], TRANSLATIONS[lang]);
-    if (!t) {
-        t = path.split('.').reduce((acc, part) => acc && acc[part], TRANSLATIONS["en"]);
-    }
-    if (!t) {
-        t = "";
-    }
+    const split = path.split(".");
+    let t = split.reduce((acc, part) => acc && acc[part], TRANSLATIONS[lang]);
+    if (!t) t = split.reduce((acc, part) => acc && acc[part], TRANSLATIONS["en"]);
+    if (!t) t = "";
     return t;
 }
 
 export async function load_json_data() {
-    await Promise.all([
-        create_worker_task('load_translations'),
-        create_worker_task('load_languages'),
-        create_worker_task('load_subdivisions')
+    const res = await Promise.all([
+        create_worker_task("load_translations"),
+        create_worker_task("load_languages"),
+        create_worker_task("load_subdivisions")
     ]);
+    TRANSLATIONS = res[0];
+    LANGUAGES = res[1];
+    SUBDIVISION_FLAGS = res[2];
 }
 
 async function create_worker_task(taskName: string): Promise<any> {
     return new Promise((resolve) => {
-        const worker = new Worker(path.join(__dirname, 'worker.ts'), { workerData: { taskName }})
-        // path.join(__dirname, 'worker.js') is just like `${__dirname}/worker.js`
-  
-        worker.once('message', (data) => resolve(data))
+        const worker = new Worker(path.join(__dirname, "worker.ts"), { workerData: { taskName } })
+        worker.once("message", (data) => resolve(data))
     })
 }
--- src/tasks/worker.ts
+++ src/tasks/worker.ts
@@ -1,47 +1,50 @@
-import { parentPort, workerData } from 'worker_threads'
+import { parentPort, workerData } from "worker_threads"
 import { readdir } from "node:fs/promises";
 import { log } from "./logs";
 
 const BASEDIR = ".";
 
-if (workerData.taskName === 'load_translations') {
-    try {
-        const dirs = await readdir(`${BASEDIR}/locales`);
-        const translations: any = {};
-  
-        for (let dir of dirs) {
-            const file = Bun.file(`${BASEDIR}/locales/${dir}/translation.json`);
-            const contents = await file.json();
-            translations[dir] = contents;
+switch (workerData.taskName) {
+    case "load_translations":
+        try {
+            const dirs = await readdir(`${BASEDIR}/locales`);
+            const translations: any = {};
+
+            for (let dir of dirs) {
+                const file = Bun.file(`${BASEDIR}/locales/${dir}/translation.json`);
+                const contents = await file.json();
+                translations[dir] = contents;
+            }
+
+            parentPort!.postMessage(translations);
+
+            log.info("Translations read");
+        } catch (err) {
+            log.error("Error getting translations", err);
         }
-  
-        parentPort!.postMessage(translations);
-  
-        log.info("Translations read");
-    } catch (err) {
-        log.error("Error getting translations", err);
-    } 
-} else if (workerData.taskName === 'load_languages') {
-    try {
-        const file = Bun.file(`${BASEDIR}/data/languages.json`);
-        const contents = await file.json();
+        break;
+    case "load_languages":
+        try {
+            const file = Bun.file(`${BASEDIR}/data/languages.json`);
+            const contents = await file.json();
 
-        parentPort!.postMessage(contents);
-  
-        log.info("Languages read");
-    } catch (err) {
-        log.error("Error getting languages", err);
-    }
-} else if (workerData.taskName === 'load_subdivisions') {
-    try {
-        const file = Bun.file(`${BASEDIR}/data/subdivisions.json`);
-        const contents = await file.json();
+            parentPort!.postMessage(contents);
 
-        parentPort!.postMessage(contents);
+            log.info("Languages read");
+        } catch (err) {
+            log.error("Error getting languages", err);
+        }
+        break;
+    case "load_subdivisions":
+        try {
+            const file = Bun.file(`${BASEDIR}/data/subdivisions.json`);
+            const contents = await file.json();
 
-        log.info("Subdivisions read");
-    } catch (err) {
-        log.error("Error getting subdivisions", err);
-    }
+            parentPort!.postMessage(contents);
+
+            log.info("Subdivisions read");
+        } catch (err) {
+            log.error("Error getting subdivisions", err);
+        }
+        break;
 }
-
