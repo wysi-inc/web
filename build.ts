1d2443328b50e41b30636201d2aa51e31dc43ace
M4rti21 <m4rti21@proton.me>
2024-10-24 11:55:48 +0200

    formatting

--- build.ts
+++ build.ts
@@ -1,28 +1,21 @@
 // Library files
 await Bun.build({
-    entrypoints: [
-        './public/js/images.js',
-        './public/js/bbcode.js',
-        './public/js/history.js',
-    ],
-    outdir: './public/js/',
+    entrypoints: ["./public/js/images.js", "./public/js/bbcode.js", "./public/js/history.js"],
+    outdir: "./public/js/",
     naming: "[dir]/[name].min.[ext]",
     minify: true, // default false
-})
+});
 
 // JS files
 await Bun.build({
-    entrypoints: [
-        './public/js/search.js',
-        './public/js/audio.js',
-    ],
-    outdir: './public/js/',
+    entrypoints: ["./public/js/search.js", "./public/js/audio.js"],
+    outdir: "./public/js/",
     naming: "[dir]/[name].min.[ext]",
     minify: {
         whitespace: true,
         identifiers: false,
         syntax: true,
     },
-})
+});
 
-export { };
+export {};
--- test.ts
+++ test.ts
@@ -5,17 +5,12 @@ await connect_osu();
 
 // console.log(await api_scores_beatmap(4644059, { mode: "osu", mods: [], type: "global" }));
 
-const scores = await api_scores_user_category(
-    17018032,
-    "best",
-    {
-        mode: "osu",
-        offset: 0,
-        limit: 100
-    }
-);
+const scores = await api_scores_user_category(17018032, "best", {
+    mode: "osu",
+    offset: 0,
+    limit: 100,
+});
 
 console.log(scores);
 
-
-export { };
+export {};
